668ed250c4b28bd8bc04fb8bf38792804ac575b6
Alexandre Costa <alexandreodpc@gmail.com>
2022-09-08 14:51:13 -0300

    fix

--- src/components/Dashboard/Hotel/HotelForm.js/index.js
+++ src/components/Dashboard/Hotel/HotelForm.js/index.js
@@ -6,7 +6,7 @@ import { HotelOption, OptionsContainer } from '../index';
 import { Instructions } from '../index';
 import Rooms from '../HotelRooms.js/Rooms';
 
-export default function HotelForm({ setBooked, userRoom }) {
+export default function HotelForm({ setBooked }) {
   const [hotels, setHotels] = useState([]);
   const [chosen, setChosen] = useState('');
 
